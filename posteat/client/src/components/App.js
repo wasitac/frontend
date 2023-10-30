740fa4caa0656bfd816293ed53cb7b1f53b9a0ca
wasitac <ghong817@gmail.com>
2023-10-30 09:10:21 +0900

    ..

--- posteat/client/src/components/App.js
+++ posteat/client/src/components/App.js
@@ -4,6 +4,7 @@ import { Layout } from "antd";
 import NavBar from "./views/NavBar/NavBar";
 import Footer from "./views/Footer/Footer";
 import LandingPage from "./views/Pages/LandingPage";
+import Detail from "./Detail/Detail";
 
 const App = () => {
   return (
@@ -13,6 +14,7 @@ const App = () => {
         <Router>
           <Routes>
               <Route path="/" element={<LandingPage />} />
+              <Route path="/store:storeId" element={<Detail />} />
           </Routes>
         </Router>
       </div>
--- posteat/client/src/components/views/Pages/LandingPage.js
+++ posteat/client/src/components/views/Pages/LandingPage.js
@@ -1,38 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 import { Content } from "antd/es/layout/layout";
-import { Row } from "antd";
-import GridCard from "../commons/GridCard";
+import { Row, Button } from "antd";
+import AntCard from "../commons/AntCard";
 
 const LandingPage = () => {
+  // state
+  const [Stores, setStores] = useState([]);
+  const [CurrentPage, setCurrentPage] = useState(1);
+
+  const loadMoreItems = ()=>{
+    
+  }
   return (
-    <div>
-      <Content
-              style={{
-                padding: "0 50px",
-              }}
-            >
-        <div style={{ width: "100%" }}>
-          {/* main image */}
-          <div style={{ width: "85%", margin: "0 auto" }}>
-            <h2>식당 리스트</h2>
-            <hr />
-            {/* movie grid card */}
-            <Row>
-              {new Array(20).fill(null).map((_, index) => {
-                return(
-                  <GridCard>
-                    path={}
-                    title={}
-                  </GridCard>
-                )
-              })}
-            </Row>
-          </div>
-          <div style={{ textAlign: "center", display: "inlineBlock" }}>
-            <button>더보기</button>
-          </div>
-        </div>
-        </Content>
+    <div style={{ width: "100%" }}>
+      {/* main image */}
+      <div style={{ width: "85%", margin: "0 auto" }}>
+        <h2>식당 리스트</h2>
+        <hr />
+        <Row gutter={[10, 10]}>
+          {/* 빈 배열을 식당목록 배열로 바꾸기 */}
+          {new Array(20).fill(null).map((value, index) => {
+            return (
+              <React.Fragment key={index + 1}>
+                <AntCard>
+                  LandingPage
+                  path={}
+                  title={}
+                </AntCard>
+              </React.Fragment>
+            );
+          })}
+        </Row>
+      </div>
+      <div style={{ textAlign: "center", display: "inlineBlock" }}>
+      <Button onClick={loadMoreItems}>더보기</Button>
+      </div>
     </div>
   );
 };
